98f37b92a8ab70f90a16b31fc4496c9e3d3ae2e9
washingtongui1 <washingtongui876@gmail.com>
2026-07-12 06:43:45 -0300

    Adicionando instalação forçada do driver ODBC

--- install_odbc.sh
+++ install_odbc.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# Instala o driver ODBC e registra no arquivo de configuração
+# Instala o driver e configura automaticamente
 apt-get update
-apt-get install -y gnupg2
+apt-get install -y gnupg2 curl unixodbc-dev
+
+# Adiciona o repositório da Microsoft
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/12/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
-apt-get install -y --allow-unauthenticated msodbcsql18
 
-# Registrar o driver no odbcinst.ini
-echo "[ODBC Driver 18 for SQL Server]" > /etc/odbcinst.ini
-echo "Description=Microsoft ODBC Driver 18 for SQL Server" >> /etc/odbcinst.ini
-echo "Driver=/opt/microsoft/msodbcsql18/lib64/libmsodbcsql-18.0.so.1.1" >> /etc/odbcinst.ini
-echo "UsageCount=1" >> /etc/odbcinst.ini
+# Instala o driver
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
+
+# Força a criação do link do odbcinst.ini usando o comando oficial do driver
+odbcinst -i -d -f /opt/microsoft/msodbcsql18/etc/odbcinst.ini
